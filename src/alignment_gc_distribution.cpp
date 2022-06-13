b520fd005ac8612c1985fbb7a1835075b1e1b461
Rish <rishvanth@gmail.com>
2022-06-13 15:10:01 -0700

    alignment_gc_distribution initial commit

---
+++ src/alignment_gc_distribution.cpp
@@ -0,0 +1,76 @@
+/*
+* alignment_gc_distribution: compute histogram of GC content
+* Copyright (C) 2022 Rishvanth Prabakar
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <unistd.h>
+
+#include "SamReader.hpp"
+#include "SamEntry.hpp"
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::vector;
+
+static string
+print_usage(const string &name) {
+  std::ostringstream oss;
+  oss << name << " [options]" << endl
+      << "\t-a aligned SAM/BAM file [required]" << endl
+      << "\t-o out file prefix [required]" << endl
+      << "\t-q minimum mapping quality to include [default: 0]" << endl;
+  return oss.str();
+}
+
+int
+main(int argc, char* argv[]) {
+  try {
+
+    string aln_file;
+    string out_prefix;
+    size_t min_mapq = 0;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "a:q:o:")) != -1) {
+      if (opt == 'a')
+        aln_file = optarg;
+      else if (opt == 'o')
+        out_prefix = optarg;
+      else if (opt == 'q')
+        min_mapq = std::stoi(optarg);
+      else
+        throw std::runtime_error(print_usage(argv[0]));
+    }
+
+    if (aln_file.empty() || out_prefix.empty()) {
+      throw std::runtime_error(print_usage(argv[0]));
+    }
+
+  }
+  catch (const std::exception &e) {
+    cerr << "ERROR: " << e.what() << endl;
+    return EXIT_SUCCESS;
+  }
+  return EXIT_FAILURE;
+}
