e5f173e99b0c1bfb59638202225c4ca498bbd1aa
Rish <rishvanth@gmail.com>
2022-08-02 11:20:39 -0700

    added Doxygen special comments in SamReader

--- src/SamReader.hpp
+++ src/SamReader.hpp
@@ -31,13 +31,43 @@
 
 using std::string;
 
+/**
+* \brief SAM/BAM file reader.
+*
+* Uses htslib to read SAM/BAM header and entries. This class only reads
+* the SAM header and entries, it uses @see SamEntry to parse them.
+*
+* @see SamEntry for parsing SAM entries.
+*/
 class SamReader {
 public:
+  /**
+  * Open a SAM/BAM file, verfies that it is in the right fromat,
+  * has a header, and initializes htslib handlers.
+  *
+  * @param [in] in_file SAM/BAM file name.
+  */
   SamReader(const string &in_file);
+  /**
+  * Closes the file and destroys htslib handlers.
+  */
   ~SamReader();
 
+  /**
+  * Reads one entry and populates 'entry' with the help of @SamEntry.
+  *
+  * @param [out] entry
+  * @return True on successfully reading an entry. False if end of file
+  * is reached. Contents of entry are not valid if return value is false.
+  * A runtime error is thrown if the enrty is not valid.
+  */
   bool read_sam_line(SamEntry &entry);
   bool read_pe_sam(SamEntry &entry1, SamEntry &entry2);
+  /**
+  * Retruns the entire header as a string.
+  *
+  * @param [out] hdr string to hold the header.
+  */
   void read_sam_header(string &hdr);
 
 private:
