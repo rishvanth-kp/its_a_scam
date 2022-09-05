56e476ba0982fa314e27895790c3d5315182e4d7
Rish <rishvanth@gmail.com>
2022-09-05 14:07:17 -0700

    added code to add sam entries and read the mismatched bases

--- src/AlignmentMismatches.hpp
+++ src/AlignmentMismatches.hpp
@@ -20,12 +20,16 @@
 #ifndef ALIGNMENT_MISMATCHES_HPP
 #define ALIGNMENT_MISMATCHES_HPP
 
+#include <string>
+#include <vector>
 
+#include "SamEntry.hpp"
+#include "GenomicStepVector.hpp"
 
 /**
 * \brief Keep track of mismatched bases and thier location.
-* 
-* Parses the MD tag and CIGAR string from a SAM entry and 
+*
+* Parses the MD tag and CIGAR string from a SAM entry and
 * tracks the location and count of mismatched bases.
 */
 class AlignmentMismatch {
@@ -39,10 +43,38 @@ public:
   */
   ~AlignmentMismatch();
 
-  
+  /**
+  * Adds the bases covered and mismates in a sam entry.
+  * The cigar string is parsed and all the matching and mismatching
+  * bases are added to coverage. Then the MD tag is parsed to find the
+  * mismatches, and the mismatched bases are recoreded. The reference base
+  * is not kept track of.
+  *
+  * @param [in] sam_entry sam entry to be added.
+  */
+  void add(const SamEntry &sam_entry);
+
+  /**
+  * After all the sam entries are added, this method is used to retrive
+  * the mismatched bases and the coverage at that base.
+  *
+  * @param [in] in_region Region to get the mismatches from.
+  * @param [out] out_region One base wide region for each mismatch in the
+  *   input region. 'aln_count' and 'mismatches' are parallel arrays
+  *   corresponding the location in 'out_region'.
+  * @param [out] aln_count Coverage at the position.
+  * @param [out] mismatches A string with all the mismatched bases at the
+  *   position. The length of this string is equal to the number of
+  *   mismatched bases.
+  */
+  void at(const GenomicRegion &in_region,
+          vector<GenomicRegion> &out_region,
+          vector<size_t> &aln_count,
+          vector<string> &mismatches);
 
 private:
-  
-}; 
+  GenomicStepVector<string> mismatches;
+  GenomicStepVector<size_t> coverage;
+};
 
 #endif
