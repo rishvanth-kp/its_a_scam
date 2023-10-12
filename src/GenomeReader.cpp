68d2f7679475c26f8f8502cc1f5f77b2251c8343
rishvanth <rishvanth@gmail.com>
2023-10-12 16:55:14 -0400

    updated genome reader

--- src/GenomeReader.cpp
+++ src/GenomeReader.cpp
@@ -17,12 +17,84 @@
 * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+
 #include "GenomeReader.hpp"
 
 using std::cout;
 using std::cerr;
 using std::endl;
+using std::string;
 
-GenomeReader::GenomeReader() {
+GenomeReader::GenomeReader(const string &in_file) {
+  read_genome(in_file);  
+}
 
+
+void 
+GenomeReader::read_genome (const string &in_file) {
+  
+  std::ifstream in(in_file);
+  if (!in)
+    throw std::runtime_error("Cannot open " + in_file);
+  
+  string line;
+  while (getline(in, line)) {
+    cout << line << endl;
+    if (line[0] == '>') {
+      chr_name.push_back(line.substr(1));
+      chr_seq.push_back(string{});
+      ++n_chr;
+    }
+    else {
+      chr_seq.back() += line;
+    }
+  }
+
+  chr_abs_pos.push_back(0);
+  for (size_t i = 0; i < n_chr; ++i) {
+    std::transform(chr_seq[i].begin(), chr_seq[i].end(),
+                   chr_seq[i].begin(), toupper);
+    
+    genome_size += chr_seq[i].length();
+    chr_abs_pos.push_back(genome_size);
+  }
+
+  cout << "genome size: " << genome_size << endl;
+  for (size_t i = 0; i < n_chr; ++i) {
+    cerr << "\t" << chr_name[i] 
+         << "\t" << chr_seq[i].length()
+         << "\t" << chr_abs_pos[i] 
+         << "\t" << chr_seq[i] << endl;
+  }
+
+  in.close();
+}
+
+
+size_t
+GenomeReader::chr_count() const { 
+  return n_chr; 
+}
+
+
+size_t 
+GenomeReader::chr_len(const size_t i) const {
+  return chr_seq[i].length();
+}
+
+
+string 
+GenomeReader::chr_tag(const size_t i) const {
+  return chr_name[i];
+}
+  
+
+std::string 
+GenomeReader::chr_substr(const size_t chr, const size_t pos,
+                         const size_t len) const {
+
+  return chr_seq[chr].substr(pos, len);
 }
--- src/GenomeReader.hpp
+++ src/GenomeReader.hpp
@@ -26,10 +26,24 @@
 
 class GenomeReader {
 public:
-  GenomeReader();
-  
+  GenomeReader(const std::string &in_file);
+
+  size_t chr_count() const;
+  size_t chr_len(const size_t i) const;
+  std::string chr_tag(const size_t i) const;
+  std::string chr_substr(const size_t chr, const size_t pos,
+                         const size_t len) const;
+ 
 private:
 
+  std::vector<std::string> chr_name;
+  std::vector<std::string> chr_seq;
+  std::vector<size_t> chr_abs_pos;
+  size_t n_chr;
+  size_t genome_size;
+  bool VERBOSE = false;
+
+  void read_genome (const std::string &in_file);
 };
 
 #endif
