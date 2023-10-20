90abe90831d0e23b2403efee22e13a4b9d9fd6a0
rishvanth <rishvanth@gmail.com>
2023-10-20 15:56:42 -0400

    add option parser

--- src/tss_enrichment.cpp
+++ src/tss_enrichment.cpp
@@ -31,9 +31,78 @@ using std::vector;
 using std::string;
 using std::unordered_map;
 
+
+static string
+print_usage (const string &name) {
+  std::ostringstream oss;
+  oss << name << " [options]" << endl
+      << "\t-a aligned SAM/BAM file [required]" << endl
+      << "\t-t TSS bed file [required]" << endl
+      << "\t-b barcode list (if empty, treated as bulk sample)" << endl
+      << "\t-o outfile prefix [required]" << endl
+      << "\t-d name split delimeter " 
+          << "[default: \":\"; ignored if -t is provided]" << endl
+      << "\t-c barcode field in name " 
+          << "[default: 7 (0 based); ignored if -t is provided]" << endl
+      << "\t-t barcode tag in SAM/BAM file [default \"\"]" << endl 
+      << "\t-q minimum mapping quality to include [default: 0]" << endl
+      << "\t-f only include if all the flags are present [default: 3]" << endl
+      << "\t-F only include if none of the flags are present [default: 3340]" 
+          << endl
+      << "\t-v verbose [default: false]" << endl;
+  return oss.str();
+}
+
 int
 main (int argc, char* argv[]) {
   try {
+
+    string aln_file;
+    string tss_file;
+    string bc_file; 
+    string out_prefix;
+
+    char bc_delim = ':';
+    uint8_t bc_col = 7;
+    string bc_tag;
+
+    size_t min_mapq = 0;
+    size_t include_all = 0x0003;
+    size_t include_none = 0x0D0C;    
+
+    bool VERBOSE = false;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "a:t:b:o:d:c:t:q:f:F:v")) != -1) {
+      if (opt == 'a')
+        aln_file = optarg;
+      else if (opt == 't')
+        tss_file = optarg;
+      else if (opt == 'b')
+        bc_file = optarg;
+      else if (opt == 'o')
+        out_prefix = optarg;
+      else if (opt == 'd')
+        bc_delim = optarg[0];
+      else if (opt == 'c')
+        bc_col = std::stoi(optarg);
+      else if (opt == 't')
+        bc_tag = optarg;
+      else if (opt == 'q')
+        min_mapq = std::stoi(optarg);
+      else if (opt == 'f')
+        include_all = std::stoi(optarg);
+      else if (opt == 'F')
+        include_none = std::stoi(optarg);
+      else if (opt == 'v')
+        VERBOSE = true;
+      else 
+        throw std::runtime_error(print_usage(argv[0]));
+    } 
+
+    if (aln_file.empty() || tss_file.empty() || out_prefix.empty()) {
+      throw std::runtime_error(print_usage(argv[0]));
+    }
 
   } 
   catch (const std::exception &e) {
