2c1e3554cfdf8073a1ae179dafaa9942f5b86cbf
rishvanth <rishvanth@gmail.com>
2023-10-24 11:07:32 -0400

    added code to get coverage around tss and output data

--- src/tss_enrichment.cpp
+++ src/tss_enrichment.cpp
@@ -23,6 +23,7 @@
 #include <fstream>
 #include <unistd.h>
 #include <unordered_map>
+#include <unordered_set>
 
 #include "SamEntry.hpp"
 #include "SamReader.hpp"
@@ -37,6 +38,7 @@ using std::endl;
 using std::vector;
 using std::string;
 using std::unordered_map;
+using std::unordered_set;
 
 
 struct TssMetadata {
@@ -173,6 +175,14 @@ main (int argc, char* argv[]) {
     vector<vector<size_t>> tss_coverage(bc_counter,
                                         vector<size_t>(2*side_dist + 1, 0));
 
+
+    // check if the cell barcode is in the name or a tag
+    // if a tag parameter is not provided, then the barcode is in the name
+    bool bc_in_tag = false;
+    if (!bc_tag.empty()) {
+      bc_in_tag = true;
+    }
+
     if (VERBOSE) {
       if (bulk_sample) {
         cerr << "\tBulk sample: " << out_prefix << endl;
@@ -188,7 +198,7 @@ main (int argc, char* argv[]) {
       cerr << "[PROCESSING TSS REGIONS]" << endl;
 
     GenomicStepVector<FeatureVector<string>> tss;
-    vector<TssMetadata> tss_metadata;
+    unordered_map<string, TssMetadata> tss_metadata;
 
     size_t tss_counter = 0;
   
@@ -211,7 +221,8 @@ main (int argc, char* argv[]) {
       metadata.start = bed_region.start;
       metadata.end = bed_region.end;
       metadata.tss_id = bed_fields[0];
-      metadata.strand = bed_fields[1][0];
+      metadata.strand = bed_fields[2][0];
+      tss_metadata[bed_fields[0]] = metadata;
 
       // keep track of number of tss
       ++tss_counter;
@@ -225,7 +236,115 @@ main (int argc, char* argv[]) {
     if (VERBOSE)
       cerr << "[PROCESSING ALIGNMENTS]" << endl;
     
+    SamReader sam_reader(aln_file);
+    SamEntry entry1, entry2;
     
+    size_t aln_count = 0;
+    size_t aln_pass_count = 0;
+    size_t in_bc_count = 0;
+  
+    while (sam_reader.read_pe_sam(entry1, entry2)) {
+      ++aln_count;
+      if (VERBOSE) {
+        if (!(aln_count % 1000000)) {
+          cerr << "\tprocessed " << aln_count << " pairs" << endl;
+        }
+      }
+
+      if (entry1.mapq >= min_mapq && entry2.mapq >= min_mapq &&
+          SamFlags::is_all_set(entry1.flag, include_all) &&
+          SamFlags::is_all_set(entry2.flag, include_all) &&
+          !SamFlags::is_any_set(entry1.flag, include_none) &&
+          !SamFlags::is_any_set(entry2.flag, include_none)) {
+
+        ++aln_pass_count;
+
+        // get the cell barcode
+        string cell_bc;
+        if (bc_in_tag) {
+          // read bc from the appropriate tag
+          SamTags::get_tag(entry1.tags, bc_tag, cell_bc);
+        }
+        else {
+          // parser the sam name to get the bc
+          vector<string> tokens;
+          split_string(entry1.qname, tokens, bc_delim);
+          if (tokens.size() > bc_col) {
+            cell_bc = tokens[bc_col];
+          }
+        }
+      
+        // check if the barcode is in list and get the barcode index
+        bool bc_found = bulk_sample;
+        size_t tss_row_index = 0;
+        if (!bulk_sample) {
+          unordered_map<string, size_t>::iterator bc_it;
+          bc_it = bc_index.find(cell_bc);
+          if (bc_it != bc_index.end()) {
+            bc_found = true;
+            tss_row_index = bc_it->second;
+          }
+        }
+ 
+        // proceed if barcode is found
+        if (bc_found) {
+          ++in_bc_count;
+
+          // find the start and end postion of the reads
+          size_t e1_start = entry1.pos - 1;
+          size_t e1_end = SamCigar::reference_end_pos(entry1);
+          size_t e2_start = entry2.pos - 1;
+          size_t e2_end = SamCigar::reference_end_pos(entry2);
+
+          // find the start and postion of the fragment
+          size_t frag_start = e1_start;
+          if (e2_start < e1_start)
+            frag_start = e2_start;
+
+          size_t frag_end = e1_end;
+          if (e2_end > e1_end) 
+            frag_end = e2_end;
+
+
+          // check if the fragment is in a TSS region
+          unordered_set<string> aligned_tss;
+          vector<pair<GenomicRegion, FeatureVector<string>>> out;
+          tss.at(GenomicRegion(entry1.rname, frag_start, frag_end), out); 
+          for (auto it = out.begin(); it != out.end(); ++it) {
+            for (size_t j = 0; j < it->second.size(); ++j) {
+              aligned_tss.insert(it->second.at(j));
+            }
+          }
+
+          for (auto it = aligned_tss.begin(); it != aligned_tss.end(); ++it) {
+            TssMetadata metadata = tss_metadata[*it];
+            for (size_t j = frag_start; j < frag_end; ++j) {
+              int tss_offset = j - metadata.start + side_dist;
+              if (tss_offset >= 0 && tss_offset <= 2*side_dist) {
+                ++tss_coverage[tss_row_index][tss_offset];
+              } 
+            }
+          }
+
+
+        }
+ 
+      }
+    }  
+
+    // write output
+    if (VERBOSE)
+      cerr << "[WRITING OUTPUT]" << endl;
+
+    std::ofstream out(out_prefix + "_tss_enrichmet.txt");
+    for (size_t i = 0; i < bc_metadata.size(); ++i) {
+      out << bc_metadata[i] << "\t";
+      for (size_t j = 0; j < tss_coverage[i].size(); ++j) {
+        out << tss_coverage[i][j] << "\t";
+      }
+      out << endl;
+    }
+    out.close();
 
   } 
   catch (const std::exception &e) {
